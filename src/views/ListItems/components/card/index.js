59a64078868cd56c1049fe680964900465e4f63b
“Nataliia <nataliia.siroukh@litslink.com>
2018-03-16 11:10:31 +0200

    Add path to the styles Card-view

--- src/views/ListItems/components/card/index.js
+++ src/views/ListItems/components/card/index.js
@@ -2,36 +2,42 @@ import React, { Component } from 'react';
 
 import '../../../../assets/styles/ListItems/components/card.css';
 
-const Card = (props) => {
-  const { card, deleteWeatherCard } = props;
+class Card extends Component {
+  constructor(props){
+    super(props);
+  }
 
-  return (
-    <div className="card-wrapper">
-      <div
-        onClick={() => deleteWeatherCard(card.location.name)}
-        className="close"
-      />
-      <div className="location">{card.location.name}</div>
-      <div className="card-last-updated" hidden="">{card.current.last_updated}</div>
-      <div className="current">
-      <div className="visual">
-        <div className="icon">
-          <img src={card.current.condition.icon} alt={'weatherIcon'}/>
+  render() {
+    const { card, deleteWeatherCard } = this.props;
+
+    return (
+      <div className="card-wrapper">
+        <div
+          onClick={() => deleteWeatherCard(card.location.name)}
+          className="close"
+        />
+        <div className="location">{card.location.name}</div>
+        <div className="card-last-updated" hidden="">{card.current.last_updated}</div>
+        <div className="current">
+        <div className="visual">
+          <div className="icon">
+            <img src={card.current.condition.icon} />
+          </div>
+          <div className="temperature">
+            <span className="value">{card.current.temp_c}</span><span className="scale">°C</span>
+          </div>
         </div>
-        <div className="temperature">
-          <span className="value">{card.current.temp_c}</span><span className="scale">°C</span>
+        <div className="description">
+          <div className="humidity">{card.current.humidity}%</div>
+          <div className="wind">
+            <span className="value">{card.current.wind_kph}km</span>
+            <span className="direction">{card.current.wind_degree}°</span>
+          </div>
+        </div>
         </div>
       </div>
-      <div className="description">
-        <div className="humidity">{card.current.humidity}%</div>
-        <div className="wind">
-          <span className="value">{card.current.wind_kph}km</span>
-          <span className="direction">{card.current.wind_degree}°</span>
-        </div>
-      </div>
-      </div>
-    </div>
-  );
+    );
+  }
 }
 
 export default Card;
